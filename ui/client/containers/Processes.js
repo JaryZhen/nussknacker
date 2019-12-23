d7edcf688c116225dac51af065b2ad0c377d9c05
Łukasz Ciołecki <lcl@touk.pl>
2019-12-23 20:58:51 +0100

    Move all icons to InlinedSvgs

--- ui/client/containers/Processes.js
+++ ui/client/containers/Processes.js
@@ -3,7 +3,6 @@ import {Table, Td, Tr} from "reactable"
 import {connect} from "react-redux"
 import HttpService from "../http/HttpService"
 import ActionsUtils from "../actions/ActionsUtils"
-import DateUtils from "../common/DateUtils"
 import LoaderSpinner from "../components/Spinner.js"
 import AddProcessDialog from "../components/AddProcessDialog.js"
 import HealthCheck from "../components/HealthCheck.js"
@@ -13,11 +12,12 @@ import ProcessUtils from "../common/ProcessUtils"
 import BaseProcesses from "./BaseProcesses"
 import {Glyphicon} from 'react-bootstrap'
 import * as  queryString from 'query-string'
-import {nkPath} from "../config";
+import {nkPath} from "../config"
 import AddProcessButton from "../components/table/AddProcessButton"
 import TableSelect from "../components/table/TableSelect"
 import SearchFilter from "../components/table/SearchFilter"
 import Date from "../components/common/Date"
+import ListState from "../components/Process/ListState"
 
 class Processes extends BaseProcesses {
   queries = {
@@ -55,19 +55,19 @@ class Processes extends BaseProcesses {
   }
 
   processNameChanged = (name, e) => {
-    const newName = e.target.value;
+    const newName = e.target.value
     this.updateProcess(name, process => process.editedName = newName)
   }
 
   changeProcessName = (process, e) => {
-    e.persist();
+    e.persist()
     if (e.key === "Enter" && process.editedName !== process.name) {
       HttpService.changeProcessName(process.name, process.editedName).then((isSuccess) => {
         if (isSuccess) {
           this.updateProcess(process.name, (process) => process.name = process.editedName)
-          e.target.blur();
+          e.target.blur()
         }
-      });
+      })
     }
   }
 
@@ -123,7 +123,7 @@ class Processes extends BaseProcesses {
           pageButtonLimit={5}
           previousPageLabel="<"
           nextPageLabel=">"
-          sortable={['name', 'category', 'modifyDate', 'createDate', 'createdBy']}
+          sortable={['name', 'category', 'modifyDate', 'createdAt', 'createdBy']}
           filterable={['name', 'category', 'createdBy']}
           hideFilterInput
           filterBy={this.state.search.toLowerCase()}
@@ -159,7 +159,11 @@ class Processes extends BaseProcesses {
                   <Date date={process.modificationDate}/>
                 </Td>
                 <Td column="status" className="status-column">
-                  <div className={this.processStatusClass(process)} title={this.processStatusTitle(process)} />
+                  <ListState
+                    process={process}
+                    state={this.getProcessState(process)}
+                    isStateLoaded={this.state.statusesLoaded}
+                  />
                 </Td>
                 <Td column="edit" className="edit-column">
                   <Glyphicon glyph="edit"
